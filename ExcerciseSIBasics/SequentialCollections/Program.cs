5aa3ac5cd82fd86d8e42564ba9b018afdc48d436
Anna Pollák <tintagomba@yahoo.co.uk>
2019-02-23 17:07:56 +0100

    Sequential collection done for now

--- ExcerciseSIBasics/SequentialCollections/Program.cs
+++ ExcerciseSIBasics/SequentialCollections/Program.cs
@@ -24,6 +24,19 @@ namespace SequentialCollections
             {
                 Console.WriteLine(element + " ");
             }
+            Console.WriteLine("Count elements: " + randomQueue.Count);
+            Console.WriteLine("Is there the word 'Second' in the queue? " + randomQueue.Contains("Second"));
+            Console.WriteLine("First element here and removed: " + randomQueue.Dequeue());
+            Console.WriteLine("Here is the hash code, watup: " + randomQueue.GetHashCode());
+            Console.WriteLine("Type of this instance, as we didn't know: " + randomQueue.GetType());
+            Console.WriteLine("Removing the first element, wich is supposedly the 'Second': " + randomQueue.Dequeue());
+            Console.WriteLine("not very clean codey, here is a foreach to show the leftovers of this excercise: ");
+            foreach (var element in randomQueue)
+            {
+                Console.WriteLine(element + " ");
+            }
+            Console.WriteLine("And here is a very useful copy of our queue as an array, enjoy: " + randomQueue.ToArray());
+            Console.WriteLine("Let's see the first element, if you forgot: " + randomQueue.Peek());
         }
     }
 }
