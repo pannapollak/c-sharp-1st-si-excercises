8dbe3764e9d68975dad4717c2707d264558d6bec
Anna Pollák <tintagomba@yahoo.co.uk>
2019-02-23 16:53:07 +0100

    second commit

--- ExcerciseSIBasics/SequentialCollections/Program.cs
+++ ExcerciseSIBasics/SequentialCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,21 @@ namespace SequentialCollections
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Queue randomQueue = new Queue();
+
+            randomQueue.Enqueue("First");
+            randomQueue.Enqueue("Second");
+            randomQueue.Enqueue("Third");
+            randomQueue.Enqueue("Fourth");
+
+            Console.WriteLine("Current queue: ");
+            foreach (var element in randomQueue)
+            {
+                Console.WriteLine(element + " ");
+            }
         }
     }
 }
