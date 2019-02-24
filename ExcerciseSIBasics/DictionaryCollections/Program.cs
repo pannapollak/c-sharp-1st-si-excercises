9c9ec699be36bbc5f30e48e4df7b3c7bf25f7681
Anna Pollák <tintagomba@yahoo.co.uk>
2019-02-24 14:26:53 +0100

    hashtable excercise completed

--- ExcerciseSIBasics/DictionaryCollections/Program.cs
+++ ExcerciseSIBasics/DictionaryCollections/Program.cs
@@ -11,8 +11,29 @@ namespace DictionaryCollections
     {
         static void Main(string[] args)
         {
-            Hashtable randomHashTable = new HashTable();
+            Hashtable randomHashTable = new Hashtable
+            {
+                { "0", "zero" },
+                { "1", "one" },
+                { "2", "two" },
+                { "3", "three" },
+                { "4", "four" },
+                { "5", "five" },
+                { "6", "six" },
+                { "7", "seven" },
+                { "8", "eight" },
+                { "9", "nine" },
+            };
 
+            string numSeries = "0375";
+            foreach (var number in numSeries)
+            {
+                string currentNumber = number.ToString();
+                if (randomHashTable.ContainsKey(currentNumber))
+                {
+                    Console.WriteLine(randomHashTable[currentNumber]);
+                }
+            }
         }
     }
 }
